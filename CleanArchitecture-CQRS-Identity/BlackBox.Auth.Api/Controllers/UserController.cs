2930ec2766c74535d43a71714e8c9c8926912073
mr-mohammadi <mr-mohammadi@amnpardaz.com>
2025-04-16 15:35:06 +0330

    add api for create user

---
+++ CleanArchitecture-CQRS-Identity/BlackBox.Auth.Api/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using BlackBox.Auth.Application.Commands.User.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlackBox.Auth.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("Create")]
+        [ProducesDefaultResponseType(typeof(int))]
+        public async Task<IActionResult> CreateUser(CreateUserCommand userCommand)
+            => Ok(await _mediator.Send(userCommand));
+    }
+}
