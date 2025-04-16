e0dbef0564d0ffc08e56afbd48f5232e053ff194
mr-mohammadi <mr-mohammadi@amnpardaz.com>
2025-04-16 14:39:42 +0330

    add create user func with identity service

---
+++ CleanArchitecture-CQRS-Identity/BlackBox.Auth.Application/Common/Interface/IIdentityService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBox.Auth.Application.Common.Interface
+{
+    public interface IIdentityService
+    {
+
+        #region UserSection
+        ValueTask<(bool isSucceed, string userId)> CreateUserAsync(string username, string password, string email,
+            string fullName, List<string> roles);
+
+        #endregion
+    }
+}
---
+++ CleanArchitecture-CQRS-Identity/BlackBox.Auth.Infrastructure/Services/IdentityService.cs
@@ -0,0 +1,50 @@
+﻿using BlackBox.Auth.Application.Common.Exception;
+using BlackBox.Auth.Application.Common.Interface;
+using BlackBox.Auth.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBox.Auth.Infrastructure.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public IdentityService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _roleManager = roleManager;
+        }
+        public async ValueTask<(bool isSucceed, string userId)> CreateUserAsync(string username, string password, string email, string fullName, List<string> roles)
+        {
+            var user = new ApplicationUser()
+            {
+                FullName = fullName,
+                UserName = username,
+                Email = email,
+            };
+
+            var result = await _userManager.CreateAsync(user, password);
+            if (!result.Succeeded)
+            {
+                throw new ValidationException(result.Errors);
+            }
+
+            var addUserRole = await _userManager.AddToRolesAsync(user, roles);
+            if (!addUserRole.Succeeded)
+            {
+                throw new ValidationException(addUserRole.Errors);
+            }
+            return(result.Succeeded, user.Id);
+        }
+    }
+}
