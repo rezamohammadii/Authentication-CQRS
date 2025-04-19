c84167377d6263d272498aec04711b4af75c92eb
mr-mohammadi <mr-mohammadi@amnpardaz.com>
2025-04-19 15:53:00 +0330

    add not found exception

---
+++ CleanArchitecture-CQRS-Identity/BlackBox.Auth.Application/Common/Exception/NotFoundException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBox.Auth.Application.Common.Exception
+{
+    public class NotFoundException : System.Exception
+    {
+        public NotFoundException() : base()
+        {
+
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public NotFoundException(string message, System.Exception exp) : base(message, exp)
+        {
+
+        }
+
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+
+        }
+    }
+}
